c00bc1a279825811f8598f8627d112059a46974d
bezi7225 <b.ezi7225@gmail.com>
2025-10-18 15:41:16 +0200

    Updated Pricing.tsx

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -7,8 +7,8 @@ export function Pricing() {
     <div className="min-h-screen bg-gray-50 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center py-12">
-  <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-    Choisissez votre plan Edukidi
+  <h2 className="text-4xl font-extrabold text-gray-900 sm:text-5xl max-w-xl mx-auto">
+    Choisissez votre plan
   </h2>
 
           <p className="mt-4 text-xl text-gray-600">
