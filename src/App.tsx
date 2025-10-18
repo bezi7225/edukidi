eeb76d041d8d4526817cf7208c90dfadf5d5d29e
bezi7225 <b.ezi7225@gmail.com>
2025-10-18 17:13:35 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <LanguageProvider>
       <Router>
-        <div className="min-h-screen bg-gradient-to-br from-blue-50 via-background to-pink-50">
+        <div className="min-h-screen pt-20">
           <Navigation />
           <Routes>
             <Route path="/" element={<HomePage />} />
